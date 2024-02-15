76da9adee192b87466620cdb76586d585f62e875
Balduino Fernando <bafernando94@gmail.com>
2024-02-15 10:12:19 +0100

    feat: Installs filament panel, database models, migrations and seeders.
    This commit, adds in the the installation of filament admin panel and
    the database ERD models with it's migrations and seeders.
    
    As for bonus installs the javascript assets using bun.sh

---
+++ database/migrations/2024_02_15_085412_create_quizzes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->bigInteger('duration')->default(45);
+            //$table->enum('type', ['public', 'private'])->default('public');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->integer('max_score')->default(20);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
